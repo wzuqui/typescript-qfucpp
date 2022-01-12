c64e454ea551b35e6720b2d75d5d0ec38cccdf86
wzuqui <willian.e.marcy@gmail.com>
2022-01-12 15:30:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-qfucpp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-qfucpp)
---
+++ index.ts
@@ -0,0 +1,6 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import * as core from '@amcharts/amcharts4/src/core';
+
+import './style.css';
